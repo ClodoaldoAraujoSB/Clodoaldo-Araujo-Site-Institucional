90d5be1aeb261d8c6e5aa05c6579063c804603e3
MetaMind07 <metaminds07@gmail.com>
2023-11-09 15:43:30 -0300

    Primeira versão completa da pagina de empresas e as empresas

--- screens/empresas.php
+++ screens/empresas.php
@@ -16,7 +16,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pagina Inicial || Clodoaldo Araújo</title>
+    <title>Empresas || Clodoaldo Araújo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <style>
@@ -28,7 +28,9 @@
       }
 
       .div_lista_empresas button {
-        
+        width: 150px;
+        height: 50px;
+        margin: 10px;
       }
 
     </style>
@@ -44,19 +46,19 @@
     <!-- LISTA DE EMPRESAS - INICIO -->
     <div class="lista_empresas">
       <div class="div_lista_empresas">
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ca3Modal"><img src="../img/2.jpg" alt="ca3" style="height: 50px;"></button>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#ca3_steelModal"><img src="../img/2.jpg" alt="ca3 steel" style="height: 50px;"></button>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#circuitModal"><img src="../img/2.jpg" alt="circuit" style="height: 50px;"></button>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#isteelModal"><img src="../img/logo-isteel.jpg" alt="isteel" style="height: 50px;"></button>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#jacModal"><img src="../img/logo-jac.jpg" alt="jac" style="height: 50px;"></button>
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#ca3Modal"><img src="../img/logo-super business.png" alt="ca3" style="height: 30px;"></button>
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#ca3_steelModal"><img src="../img/logo-super business.png" alt="ca3 steel" style="height: 30px;"></button>
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#circuitModal"><img src="../img/logo-super business.png" alt="circuit" style="height: 30px;"></button>
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#isteelModal"><img src="../img/logo-isteel.png" alt="isteel" style="height: 30px;"></button>
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#jacModal"><img src="../img/logo-jac.png" alt="jac" style="height: 30px;"></button>
       </div>
       <br>
       <div class="div_lista_empresas">
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#point_pixelModal"><img src="../img/2.jpg" alt="Point Pixel" style="height: 50px;"></button>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#rota_da_inovacaoModal"><img src="../img/logo-rota.jpg" alt="Rota da Inovaçao" style="height: 50px;"></button>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#spyder_raceModal"><img src="../img/logo-spyder.jpg" alt="Spyder Race" style="height: 50px;"></button>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#startup_schoolModal"><img src="../img/logo-startup.jpg" alt="Startup School" style="height: 50px;"></button>
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#super_businessModal"><img src="../img/logo-super business.jpg" alt="Super Business" style="height: 50px;"></button>
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#point_pixelModal"><img src="../img/logo-super business.png" alt="Point Pixel" style="height: 30px;"></button>
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#rota_da_inovacaoModal"><img src="../img/logo-rota.png" alt="Rota da Inovaçao" style="height: 30px;"></button>
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#spyder_raceModal"><img src="../img/logo-spyderrace.png" alt="Spyder Race" style="height: 30px;"></button>
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#startup_schoolModal"><img src="../img/logo-startup.png" alt="Startup School" style="height: 30px;"></button>
+        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#super_businessModal"><img src="../img/logo-super business.png" alt="Super Business" style="height: 30px;"></button>
       </div>
     </div>
     <!-- LISTA DE EMPRESAS - FIM -->
--- screens/super_business.php
+++ screens/super_business.php
@@ -1,23 +1,45 @@
+<style>
+
+  #btn-empresa {
+    border: 0px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #div_centro_titulo {
+    width: 30%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #div_lateral_titulo {
+    width: 40%;
+  }
+
+</style>
+
 <!-- MODAL - INICIO -->
 <div class="modal fade" id="super_businessModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <div>
-                <img src="../img/2.jpg" alt="Imagem 2" style="height: 17px;">
+      <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content bg-dark">
+          <div class="modal-header" style="border: 0px;">
+            <div id="div_lateral_titulo">
+                <img src="../img/logo-super business.png" alt="Logo Super Business" style="height: 30px;">
             </div>
-            <div>
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Super Business</h1>
+            <div id="div_centro_titulo">
+                <h1 class="modal-title fs-5 text-light" id="exampleModalLabel">Super Business</h1>
             </div>
-            <div>
+            <div id="div_lateral_titulo" style="text-align: right;">
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
           </div>
-          <div class="modal-body">
+          <div class="modal-body text-light" style="text-align: center;">
             ...
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary">Save changes</button>
+          <div class="modal-footer" id="btn-empresa">
+            <button type="button" class="btn btn-light text-dark">Save changes</button>
           </div>
         </div>
       </div>
