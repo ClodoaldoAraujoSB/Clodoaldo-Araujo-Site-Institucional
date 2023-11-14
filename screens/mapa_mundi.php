1218514fb32d090cecb302922bba4d02ee388e10
RafaelHSLemes <rafaelhslemes@gmail.com>
2023-11-14 10:12:14 -0300

    Estruturação da página de "Países que visitei"

---
+++ screens/mapa_mundi.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pagina Inicial || Clodoaldo Araújo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <body>
+    <!-- MENU SUPERIOR -->
+    <?php include("header.php"); ?>
+
+    <div class="row g-0 text-center" style="margin-right: 130px">
+        <div class="col-sm-6 col-md-8"><img src="../img/mapamundilaranja.avif"></div>
+        <div class="col-6 col-md-4" style="margin-top: 50px;">Lorem ipsum dolor sit amet. Et consectetur ullam aut omnis autem sed fuga tempora non laudantium accusantium qui illum quis 33 consequatur asperiores. Id accusamus dolor qui facere dolorum ut blanditiis sint sit voluptates quos aut accusamus sint et maxime rerum quo autem nemo. Aut ipsum vitae qui galisum soluta 33 ducimus amet et optio dicta ex quidem eveniet aut reprehenderit consequatur ea aliquam quaerat.
+
+At necessitatibus molestiae rem illum velit ea harum repellendus sit nisi eius et dolor voluptates. Aut dolorum aliquid qui commodi natus et sint reprehenderit est ratione rerum.
+
+Quo omnis reiciendis rem dolores fuga rem repellendus ipsa et earum animi. Id dolores autem non maiores blanditiis et dolores harum.
+</div>
+    </div>
+
+    <!-- RODAPÉ -->
+    <?php include("footer.php"); ?>
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
