414401ab74eee9476cbd79b3dd438e875c358ec7
A5Rezende <antoniororlandini@gmail.com>
2023-11-16 15:12:02 -0300

    Pagina de ferramentas do ebook criada

--- screens/ferramentas_ebook.php
+++ screens/ferramentas_ebook.php
@@ -2,6 +2,11 @@
 session_start();
 if(isset($_SESSION['autorizado']) && $_SESSION['autorizado'] == true) {
     // Página permitida
+
+    include("../bd/bd.php");
+    $sql_ferramenta = $conn->query("SELECT * FROM ferramentas");
+    $conn->close();
+
     ?>
 
     <!doctype html>
@@ -11,10 +16,61 @@ if(isset($_SESSION['autorizado']) && $_SESSION['autorizado'] == true) {
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Ferramentas Gratuitas || Clodoaldo Araújo</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+        <style>
+
+            .container-list {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-wrap: wrap;
+            }
+
+        </style>
+
+        <script>
+
+            function mostrarObrigado() {
+                alert("Obrigado!!");
+            }
+
+        </script>
+    
     </head>
     <body>
         <!-- MENU SUPERIOR -->
         <?php include("header.php"); ?>
+
+        <!-- TITULO -->
+        <h1 style="text-align: center">Lista de Ferramentas</h1>
+
+        <!-- LISTA DE FERRAMENTAS -->
+        <div class="container">
+            <div class="container-list">
+            <?php
+            foreach ($sql_ferramenta as $ferramenta) {
+            ?>
+
+                <div class="card mb-3" style="width: 70%;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="../<?php echo $ferramenta["arquivo"] ?>" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body" id="card-ferramenta">
+                                <h5 class="card-title"> <?php echo $ferramenta["nome"]; ?> </h5>
+                                <p class="card-text"> <?php echo $ferramenta["descricao"]; ?> </p>
+                                <a href="../<?php echo $ferramenta["arquivo"] ?>" download="<?php echo $ferramenta["nome_arquivo"] ?>" onclick="mostrarObrigado()" class="btn btn-primary">Baixe aqui</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+            <?php
+            }
+            ?>
+            </div>
+        </div>
 
         <!-- RODAPÉ -->
         <?php include("footer.php"); ?>
