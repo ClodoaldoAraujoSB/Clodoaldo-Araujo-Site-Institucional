572088d099e16fc6fb6831974488c427ad89f18a
RafaelHSLemes <rafaelhslemes@gmail.com>
2023-11-17 15:39:16 -0300

    Estruturação da tela de uploads de ferramentas

--- admin/ferramentas.php
+++ admin/ferramentas.php
@@ -1,11 +1,52 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pagina Inicial || Clodoaldo Araújo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <title>Envio de Arquivo</title>
 </head>
-  <body>
-    <!-- MENU SUPERIOR -->
-    <?php include("header.php"); ?>
+<body>
+  <?php //include('header.php'); ?>
+
+    <h2>Envio de Arquivo</h2>
+
+    <?php
+    include('../bd/bd.php');
+    // Verifica se o formulário foi enviado
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Obtém os dados do formulário
+        $nome = $_POST["nome"];
+        $descricao = $_POST["descricao"];
+
+        // Configurações para o upload do arquivo
+        $diretorioDestino = "../ferramentas/";
+        $caminhoCompleto = $diretorioDestino . basename($_FILES["arquivo"]["name"]);
+
+        // Move o arquivo para o diretório de destino
+        if (move_uploaded_file($_FILES["arquivo"]["tmp_name"], $caminhoCompleto)) {
+            // Agora você pode armazenar o caminho no banco de dados
+            // Exemplo: Você precisa configurar a conexão com o banco de dados antes de usar a seguinte linha
+            // $conexao = new mysqli("localhost", "usuario", "senha", "banco_de_dados");
+            $query = "INSERT INTO ferramentas (nome, descricao, arquivo) VALUES ('$nome', '$descricao', '$caminhoCompleto')";
+            $conn->query($query);
+            // Lembre-se de ajustar os detalhes da conexão e da consulta conforme necessário
+            echo "Arquivo enviado com sucesso!";
+        } else {
+            echo "Erro ao enviar o arquivo.";
+        }
+    }
+    ?>
+    <br>
+    <form method="post" enctype="multipart/form-data">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" required><br>
+
+        <label for="descricao">Descrição:</label>
+        <input type="text" name="descricao" required><br>
+
+        <label for="arquivo">Arquivo:</label>
+        <input type="file" name="arquivo" required><br>
+
+        <input type="submit" value="Enviar Arquivo">
+    </form>
+</body>
+</html>
