adc859f4e024144caa78895601a8774beba240a6
A5Rezende <antoniororlandini@gmail.com>
2023-11-22 12:48:33 -0300

    menu's do admin e do site modificados

--- admin/header.php
+++ admin/header.php
@@ -5,67 +5,58 @@
   <head>
   <title>Admin || Clodoaldo Araújo</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+  <style>
+
+    .custom-navbar {
+      background-color: #c10109;
+    }
+
+  </style>
+
 </head>
 <body>
-<nav class="navbar navbar-light bg-light fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Admin</a>
+<nav class="navbar navbar-dark fixed-top custom-navbar">
+  <div class="container-fluid custom-navbar">
+    <a class="navbar-brand" href="#">Admin - Clodoaldo Araújo</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvaslightNavbar" aria-controls="offcanvaslightNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="offcanvas offcanvas-end text-bg-light" tabindex="-1" id="offcanvaslightNavbar" aria-labelledby="offcanvaslightNavbarLabel">
-      <div class="offcanvas-header">
+    <div class="offcanvas offcanvas-end text-light custom-navbar" tabindex="-1" id="offcanvaslightNavbar" aria-labelledby="offcanvaslightNavbarLabel">
+      <div class="offcanvas-header custom-navbar">
         <h5 class="offcanvas-title" id="offcanvaslightNavbarLabel">Menu do Admin</h5>
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
-      <div class="offcanvas-body">
+      <div class="offcanvas-body custom-navbar">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a href="home.php"><button type="button" class="btn btn-outline-secondary">Início</button></a>
+            <a href="home.php"><button type="button" class="btn btn-outline-light">Início</button></a>
           </li>
           <br>
           <li class="nav-item">
-            <a href="leads.php"><button type="button" class="btn btn-outline-secondary">Leads</button></a>
+            <a href="leads.php"><button type="button" class="btn btn-outline-light">Leads</button></a>
           </li>
           <br>
           <li class="nav-item">
-            <a href="contatos.php"><button type="button" class="btn btn-outline-secondary">Contatos</button></a>
+            <a href="contatos.php"><button type="button" class="btn btn-outline-light">Contatos</button></a>
           </li>
           <br>
           <li class="nav-item">
-            <a href="ferramentas.php"><button type="button" class="btn btn-outline-secondary">Ferramentas</button></a>
+            <a href="ferramentas.php"><button type="button" class="btn btn-outline-light">Ferramentas</button></a>
           </li>
           <br>
           <li class="nav-item">
-            <a href="imagens.php"><button type="button" class="btn btn-outline-secondary">Imagens</button></a>
+            <a href="imagens.php"><button type="button" class="btn btn-outline-light">Imagens</button></a>
           </li>
           <br>
           <li class="nav-item">
-            <a href="videos.php"><button type="button" class="btn btn-outline-secondary">Videos</button></a>
+            <a href="videos.php"><button type="button" class="btn btn-outline-light">Videos</button></a>
           </li>
-          <!--<li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu dropdown-menu-light">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>-->
         </ul>
-        <!--<form class="d-flex mt-3" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-success" type="submit">Search</button>
-        </form>-->
       </div>
     </div>
   </div>
 </nav>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
