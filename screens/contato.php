b90458262f4b5e62510a5a155c8c4d6a150719fc
A5Rezende <antoniororlandini@gmail.com>
2024-01-24 12:52:04 -0300

    Alteração dos relatorios da pagina inicial

--- screens/contato.php
+++ screens/contato.php
@@ -130,10 +130,7 @@
                     $idade = $_POST["idade"];
                     $comentario = $_POST["comentario"];
 
-                    date_default_timezone_set('America/Sao_Paulo');
-                    $data_envio = date('d-m-Y H:i:s');
-
-                    $conn->query("INSERT INTO contato (nome, email, telefone, cidade, idade, comentario, data_envio) VALUES ( '$nome', '$email', '$telefone', '$cidade', '$idade', '$comentario', '$data_envio')");
+                    $conn->query("INSERT INTO contato (nome, email, telefone, cidade, idade, comentario) VALUES ( '$nome', '$email', '$telefone', '$cidade', '$idade', '$comentario')");
                     $conn->close();
 
                     echo '<script>alert("Formulário enviado com sucesso!");</script>';
