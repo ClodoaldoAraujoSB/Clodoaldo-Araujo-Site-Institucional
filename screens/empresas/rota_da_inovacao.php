5f3a4350351a9aba0c4725f92ec81b59f6bdf3a5
ClodoaldoAraujo <84418988+RafaelHSLemes@users.noreply.github.com>
2024-02-15 13:34:20 -0300

    Alteração de texto da Rota da inovação

--- screens/empresas/rota_da_inovacao.php
+++ screens/empresas/rota_da_inovacao.php
@@ -44,7 +44,13 @@
             </div>
           </div>
           <div class="modal-body text-light" style="text-align: center;">
-          Apresentada pelo empresário Clodoaldo Araújo, vencedor da quinta temporada do programa Aprendiz – O Sócio. Na série “Rota da Inovação”, o empreendedor mostrará novidades, passando pelos mais diversos campos de pesquisa: arquitetura, negócios, varejo, engenharia, entre outros, por meio de viagens realizadas.
+          O programa Rota da Inovação representa uma constante busca por ideias, empresas e produtos que transformam o mundo. Por trás dessas inovações, encontramos indivíduos apaixonados pela busca constante de soluções, identificando problemas e dedicando-se incansavelmente a superá-los.
+Hoje, a inovação tornou-se essencial em todas as esferas da vida, permeando a saúde, meio ambiente, tecnologia, transporte, educação, design, cultura e alimentação, tanto em iniciativas públicas quanto privadas.
+O projeto Rota da Inovação já traçou percursos que conduziram a alguns dos países mais desenvolvidos do mundo, destacando-se no pensamento inovador. Com visitas a mais de 60 países, cada um oferece singularidades e expertise em áreas específicas, tornando-se fonte valiosa de aprendizado para o restante do mundo.
+Ao explorar diversos países, Clodoaldo Araújo, testemunhou as peculiaridades e a expertise de cada local. Na Suíça, por exemplo, percorreu cidades como Zurique, Berna, Lausanne e Interlaken, conhecendo as maiores inovações do país que ostenta o título de mais inovador do planeta, segundo a Cornell University.
+Na Alemanha, suas visitas abrangeram inovações na hotelaria, transporte, administração de desastres naturais, inteligência artificial, higiene sanitária e até em museus. Berlim e Hamburgo se destacaram como dois epicentros repletos de novidades e ideias incríveis, prontas para serem compartilhadas com o mundo.
+A Rota da Inovação continua sua jornada, inspirando e conectando mentes criativas em todo o mundo, fortalecendo a crença de que a inovação é o catalisador para um futuro mais promissor.
+
           </div>
           <div class="modal-footer" id="btn-empresa">
             <a href="https://rotadainovacao.com.br/" target="_blank" rel="noopener noreferrer"><button type="button" class="btn btn-light text-dark">Saiba Mais</button></a>
